d94243253feb6ba09c23e5b5740f0874ab12b7f7
Joyce <joyce.santos@autbank.com.br>
2022-01-07 18:42:52 -0300

    refactors e webpack

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { MakeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import { MakeLoginValidation } from './login-validation-factory'
+import { MakeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/save-access-token-factory'
 
 export const MakeLogin: React.FC = () => {
   return (
         <Login
             authentication={MakeRemoteAuthentication()}
             validation={MakeLoginValidation()}
+            saveAccessToken={MakeLocalSaveAccessToken()}
         />
   )
 }
