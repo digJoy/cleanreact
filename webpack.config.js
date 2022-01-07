e54994e665eecfcf5a7a4de5168526bcc531403a
Joyce <joyce.santos@autbank.com.br>
2022-01-07 18:44:23 -0300

    refactors e webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const {DefinePlugin} = require('webpack')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -13,7 +13,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
-      '@': path.join(__dirname, 'src')
+    '@': path.join(__dirname, 'src')
     }
   },
   module: {
