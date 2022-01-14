dd016b73dc441326398febabfb8ab044498dc339
Joyce <joyce.santos@autbank.com.br>
2022-01-14 17:39:44 -0300

    refactor: refactoring

--- src/data/test/mock-http-post.ts
+++
@@ -1,7 +0,0 @@
-import { HttpPostParams } from '../protocols/http'
-import * as faker from 'faker'
-
-export const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement
-})
